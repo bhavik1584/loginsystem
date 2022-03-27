fba8b4cffb9ae1fdb14dd50f0506ef489023d416
bhavik <bhavik19s2000@gmail.com>
2022-03-27 18:48:19 +0530

    initial commit

--- loginsystem/login.php
+++ loginsystem/login.php
@@ -15,7 +15,10 @@ if($_SERVER['REQUEST_METHOD']=='POST')
     $username=$_POST['username'];
     $password=$_POST['password'];
    
-        
+
+
+    
+      
 
    
         $serch="SELECT * FROM `login`.`logindatabase` WHERE `username`='$username' AND `password`='$password'";
